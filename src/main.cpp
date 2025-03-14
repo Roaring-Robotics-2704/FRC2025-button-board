f3b503fc0b9c386c5b4a4ed99cf1c0493d810bbf
Nova <yt.nogo21.pro@gmail.com>
2025-03-13 20:33:02 -0500

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,133 @@
-#include <Arduino.h>
-#include <PicoGamepad.h>
+/*********************************************************************
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
 
+ MIT license, check LICENSE for more information
+ Copyright (c) 2021 NeKuNeKo for Adafruit Industries
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
 
-PicoGamepad gamepad;
+#include "Adafruit_TinyUSB.h"
+#include "Adafruit_Seesaw.h"
+#include <seesaw_neopixel.h>
 
-// 16 bit integer for holding input values
-int val;
+#define  DEFAULT_I2C_ADDR 0x3A
 
-void setup() {  
+#define  SWITCH1  18  // PA01
+#define  SWITCH2  19 // PA02
+#define  SWITCH3  20 // PA03
+#define  SWITCH4  2 // PA06
+#define  PWM1  12  // PC00
+#define  PWM2  13 // PC01
+#define  PWM3  0 // PA04
+#define  PWM4  1 // PA05
+
+/* This sketch demonstrates USB HID gamepad use.
+ * This sketch is only valid on boards which have native USB support
+ * and compatibility with Adafruit TinyUSB library. 
+ * For example SAMD21, SAMD51, nRF52840.
+ * 
+ * Make sure you select the TinyUSB USB stack if you have a SAMD board.
+ * You can test the gamepad on a Windows system by pressing WIN+R, writing Joy.cpl and pressing Enter.
+ */
+
+// HID report descriptor using TinyUSB's template
+// Single Report (no ID) descriptor
+uint8_t const desc_hid_report[] = {
+    TUD_HID_REPORT_DESC_GAMEPAD()
+};
+
+// USB HID object
+Adafruit_USBD_HID usb_hid;
+
+// Report payload defined in src/class/hid/hid.h
+// - For Gamepad Button Bit Mask see  hid_gamepad_button_bm_t
+// - For Gamepad Hat    Bit Mask see  hid_gamepad_hat_t
+hid_gamepad_report_t gp;
+
+void setup() {
+  // Manual begin() is required on core without built-in support e.g. mbed rp2040
+  if (!TinyUSBDevice.isInitialized()) {
+    TinyUSBDevice.begin(0);
+  }
+
   Serial.begin(115200);
-  
-  pinMode(LED_BUILTIN, OUTPUT);
 
-  // X Potentiometer on pin 26
-  pinMode(26, INPUT);
-  // Y Potentiometer on pin 27
-  pinMode(27, INPUT);
+  // Setup HID
+  usb_hid.setPollInterval(2);
+  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+  usb_hid.begin();
 
-  // Button on pin 
-  pinMode(28, INPUT_PULLUP);
+  // If already enumerated, additional class driverr begin() e.g msc, hid, midi won't take effect until re-enumeration
+  if (TinyUSBDevice.mounted()) {
+    TinyUSBDevice.detach();
+    delay(10);
+    TinyUSBDevice.attach();
+  }
+
+  Serial.println("Adafruit TinyUSB HID Gamepad example");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  delay(10); // delay in loop to slow serial output
+  
+  // Reverse x/y values to match joystick orientation
+  int x = 1023 - ss.analogRead(14);
+  int y = 1023 - ss.analogRead(15);
+
+#if defined(IRQ_PIN)
+  if(!digitalRead(IRQ_PIN)) {
+    return;
+  }
+#endif
+
+
+
+  #ifdef TINYUSB_NEED_POLLING_TASK
+  // Manual call tud_task since it isn't called by Core's background
+  TinyUSBDevice.task();
+  #endif
+
+  // not enumerated()/mounted() yet: nothing to do
+  if (!TinyUSBDevice.mounted()) {
+    return;
+  }
+
+//  // Remote wakeup
+//  if ( TinyUSBDevice.suspended() && btn )
+//  {
+//    // Wake up host if we are in suspend mode
+//    // and REMOTE_WAKEUP feature is enabled by host
+//    TinyUSBDevice.remoteWakeup();
+//  }
+
+  if (!usb_hid.ready()) return;
+
+  // Reset buttons
+  Serial.println("No pressing buttons");
+  gp.buttons = 0;
+  usb_hid.sendReport(0, &gp, sizeof(gp));
+  delay(2000);
+
+
+  // Test buttons (up to 32 buttons)
+  for (int i = 0; i < 32; ++i) {
+    Serial.print("Pressing button ");
+    Serial.println(i);
+    gp.buttons = (1U << i);
+    usb_hid.sendReport(0, &gp, sizeof(gp));
+    delay(1000);
+  }
+
+
+  // Random touch
+  Serial.println("Random touch");
+  gp.buttons = random(0, 0xffff);
+  usb_hid.sendReport(0, &gp, sizeof(gp));
+  delay(2000);
+
+  // */
 }
